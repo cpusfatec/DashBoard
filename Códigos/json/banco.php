05de429bae52ee2ea120be5d4c623267800220e4
Higor-SM <72944799+Higor-SM@users.noreply.github.com>
2021-05-16 23:56:59 -0300

    Delete Códigos/json directory

--- Códigos/json/banco.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = new PDO("mysql:host=sql102.epizy.com;dbname=epiz_26915201_banco", 'epiz_26915201', 'YwQ5pfjia0');
-?>
--- Códigos/json/verifica_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if($_SESSION['nivel'] != "ok")  {
-	header('Location: index.php');
-	exit();
-}
